34ed24471b3df21c2ab1d15d2630f689c997e8bf
Minh Luc Van <minh.lucvan@ncc.asia>
2021-10-11 14:52:51 +0700

    update footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -12,11 +12,11 @@ const Footer = () => (
   >
     <div className="container">
       <p>
-        A GatsbyJS Starter for Netlify CMS, Made with{" "}
+        Sharing with {" "}
         <span className="icon -love">
           <RiHeart2Line />
         </span>{" "}
-        by <Link to="/">Stackrole.com</Link>
+        by <Link to="/">@minhlucvan</Link>
       </p>
     </div>
   </footer>
