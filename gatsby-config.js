c29169263432ff7554739f314dbac10479ac66f3
Minh Luc Van <minh.lucvan@ncc.asia>
2021-10-13 16:01:38 +0700

    update config

--- gatsby-config.js
+++ gatsby-config.js
@@ -108,5 +108,22 @@ module.exports = {
       },
     },
     "gatsby-plugin-offline",
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        // Add any options here
+      },
+    },
+    {
+      resolve: `gatsby-plugin-emotion`,
+      options: {
+        // Accepts the following options, all of which are defined by `@emotion/babel-plugin` plugin.
+        // The values for each key in this example are the defaults the plugin uses.
+        sourceMap: true,
+        autoLabel: "dev-only",
+        labelFormat: `[local]`,
+        cssPropOptimization: true,
+      },
+    },
   ],
 }
